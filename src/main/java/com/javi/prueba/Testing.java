1f2fd87b327bf073804c56aeb0ed7dbb6ea61f8b
Spkspeed <spkspeed@gmail.com>
2021-01-30 19:57:27 -0300

    tarea13

---
+++ src/main/java/com/javi/prueba/Testing.java
@@ -0,0 +1,17 @@
+package com.javi.prueba;
+
+public class Testing {
+    Object message(){
+        return "hello";
+    }
+    public static void main(String[] args) {
+        System.out.println(new Testing().message());
+        System.out.println(new main2().message());
+    }
+}
+
+class main2 extends Testing{
+    Object message(){
+        return "world";
+    }
+}
